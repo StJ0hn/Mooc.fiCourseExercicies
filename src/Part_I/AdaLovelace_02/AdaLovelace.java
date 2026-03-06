8cf51ac0f9117796933ac1ad5644badd8f50654f
john <jonhmiguel138@gmail.com>
2026-03-06 18:06:40 -0300

    resolution of question 02_Ada Lovelace

---
+++ src/Part_I/AdaLovelace_02/AdaLovelace.java
@@ -0,0 +1,7 @@
+package Part_I.AdaLovelace_02;
+
+public class AdaLovelace {
+    static void main(String[] args) {
+        System.out.println("Ada Lovelace");
+    }
+}
--- src/Part_I/Sandbox_01/Sandbox.java
+++ src/Part_I/Sandbox_01/Sandbox.java
@@ -1,3 +1,4 @@
+package Part_I.Sandbox_01;
 public class Sandbox {
     public static void main(String[] args) {
         System.out.println("Hello World!");
