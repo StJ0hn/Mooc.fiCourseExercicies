cfbe9223c408cde6196e641aa716c560d63f20bd
john <jonhmiguel138@gmail.com>
2026-03-08 15:33:41 -0300

    resolution of question 6 - HiAdaLovelace

---
+++ src/Part_I/Ex_06_HiAdaLovelace/HiAdaLovelace.java
@@ -0,0 +1,8 @@
+package Part_I.Ex_06_HiAdaLovelace;
+
+public class HiAdaLovelace {
+    public static void main(String[] args) {
+        String name = "Ada Lovelace";
+        System.out.println("Hi " + name + "!");
+    }
+}
