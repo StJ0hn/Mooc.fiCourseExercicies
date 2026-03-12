4d886b41103cbb9b6af750442f323cf353bd053b
john <jonhmiguel138@gmail.com>
2026-03-12 20:41:17 -0300

    feat: resolution of question 19 - AdditionFormula

---
+++ src/Part_I/Ex_19_AdditionFormula/AdditionFormula.java
@@ -0,0 +1,17 @@
+package Part_I.Ex_19_AdditionFormula;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class AdditionFormula {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Give the first number:");
+        int first = Integer.parseInt(sc.nextLine());
+        System.out.println("Give the second number:");
+        int second = Integer.parseInt(sc.nextLine());
+        int sum = first + second;
+        System.out.println(first + " + " + second + " = " + sum);
+    }
+}
