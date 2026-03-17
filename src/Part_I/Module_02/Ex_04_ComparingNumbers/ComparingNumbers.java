46310178b7383bb44bd67848c76255bc6c48a7a2
john <jonhmiguel138@gmail.com>
2026-03-17 18:33:48 -0300

    feat: resolution of question 4 - ComparingNumbers of module 02

--- src/Part_I/Module_02/Ex_04_ComparingNumbers/ComparingNumbers.java
+++ src/Part_I/Module_02/Ex_04_ComparingNumbers/ComparingNumbers.java
@@ -1,4 +1,18 @@
 package Part_I.Module_02.Ex_04_ComparingNumbers;
 
+import java.util.Scanner;
+
 public class ComparingNumbers {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int firstNumber = Integer.parseInt(sc.nextLine());
+        int secondNumber = Integer.parseInt(sc.nextLine());
+        if (firstNumber > secondNumber){
+            System.out.printf("%d is greater than %d", firstNumber, secondNumber);
+        } else if (secondNumber > firstNumber) {
+            System.out.printf("%d is greater than %d", secondNumber, firstNumber);
+        } else {
+            System.out.printf("%d is equal to %d", firstNumber, secondNumber);
+        }
+    }
 }
