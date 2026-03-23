c4fd0c41d467b4967bb57daca3a2d64432498f13
john <jonhmiguel138@gmail.com>
2026-03-23 14:46:04 -0300

    feat: resolution of question 19 - Factorial of module 02

--- src/Part_I/Module_02/Ex_19_Factorial/Factorial.java
+++ src/Part_I/Module_02/Ex_19_Factorial/Factorial.java
@@ -1,4 +1,16 @@
 package Part_I.Module_02.Ex_19_Factorial;
 
+import java.util.Scanner;
+
 public class Factorial {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Give a number: ");
+        int numberFact = Integer.parseInt(sc.nextLine());
+        int fact = 1;
+        for (int i = 1; i <= numberFact; i++) {
+            fact *= i;
+        }
+        System.out.print("Factorial: " + fact);
+    }
 }
