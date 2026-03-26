bfe0dd704d34386bc760faa3021bdc65ca8537c0
john <jonhmiguel138@gmail.com>
2026-03-26 12:08:42 -0300

    feat: resolution of question 21 - InAHoleInTheGround

---
+++ src/Part_I/Module_02/Ex_21_InAHoleInTheGround/InAHoleInTheGround.java
@@ -0,0 +1,10 @@
+package Part_I.Module_02.Ex_21_InAHoleInTheGround;
+
+public class InAHoleInTheGround {
+    public static void main(String[] args) {
+        printText();
+    }
+    public static void printText(){
+        System.out.println("In a hole in the ground there lived a method");
+    }
+}
