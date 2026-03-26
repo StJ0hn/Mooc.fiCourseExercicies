29e0651b757204740ee6b65815ef95561a32d765
john <jonhmiguel138@gmail.com>
2026-03-26 15:05:18 -0300

    feat: resolution of question 25 - Division

---
+++ src/Part_I/Module_02/Ex_25_Division/Division.java
@@ -0,0 +1,12 @@
+package Part_I.Module_02.Ex_25_Division;
+
+public class Division {
+    public static void main(String[] args) {
+        division(3, 5);
+    }
+
+    public static void division(int numberOne, int numberTwo) {
+        double result = (double) (numberOne) / numberTwo;
+        System.out.println(result);
+    }
+}
