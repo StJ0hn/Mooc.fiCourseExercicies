dff5fdeb25f085a063d748425527446de43a4ae9
john <jonhmiguel138@gmail.com>
2026-03-27 11:34:17 -0300

    feat: resolution of question 27 - numberUno

---
+++ src/Part_I/Module_02/Ex_27_NumberUno/NumberUno.java
@@ -0,0 +1,12 @@
+package Part_I.Module_02.Ex_27_NumberUno;
+
+public class NumberUno {
+    public static void main(String[] args) {
+        System.out.println(numberUno());
+    }
+
+    public static int numberUno(){
+        int one = 1;
+        return one;
+    }
+}
