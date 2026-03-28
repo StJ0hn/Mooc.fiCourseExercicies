168afc97b3d367216e5bb3b96b881ba54df5e077
john <jonhmiguel138@gmail.com>
2026-03-28 00:05:05 -0300

    feat: resolution of question 34 - AdvancedAstrology

--- src/Part_I/Module_02/Ex_33_StarSign/StarSign.java
+++ src/Part_I/Module_02/Ex_33_StarSign/StarSign.java
@@ -5,7 +5,7 @@ public class StarSign {
         for (int i = 0; i < number; i++) {
             System.out.print("*");
         }
-        System.out.println(" ");
+        System.out.println();
     }
 
     public static void printSquare(){
---
+++ src/Part_I/Module_02/Ex_34_AdvancedAstrology/AdvancedAstrology.java
@@ -0,0 +1,38 @@
+package Part_I.Module_02.Ex_34_AdvancedAstrology;
+
+public class AdvancedAstrology {
+    public static void printStars(int number) {
+        for (int i = 0; i < number; i++) {
+            System.out.print("*");
+        }
+        System.out.println();
+    }
+
+    public static void printSpaces(int number){
+        for (int i = 1; i <= number; i++) {
+            System.out.print(" ");
+        }
+    }
+
+    public static void printTriangle(int size){
+        for (int i = 1; i <= size; i++) {
+            printSpaces(size - i);
+            printStars(i);
+        }
+    }
+
+    public static void christmasTree(int height){
+        for (int i = 1; i <= height; i++) {
+            printSpaces(height - i);
+            printStars((i*2) - 1);
+        }
+        printSpaces(height - 2);
+        printStars(3);
+        printSpaces(height - 2);
+        printStars(3);
+    }
+
+    public static void main(String[] args) {
+        christmasTree(4);
+    }
+}
