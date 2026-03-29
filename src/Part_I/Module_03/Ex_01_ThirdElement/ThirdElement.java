dd3fc2f359535a4d9d49e99e282283f9039dd5e2
john <jonhmiguel138@gmail.com>
2026-03-29 16:43:41 -0300

    feat: resolution of question 1 - ThirdElement of module 3

---
+++ src/Part_I/Module_03/Ex_01_ThirdElement/ThirdElement.java
@@ -0,0 +1,21 @@
+package Part_I.Module_03.Ex_01_ThirdElement;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class ThirdElement {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<String> list = new ArrayList<>();
+
+        while (true) {
+            String input = scanner.nextLine();
+
+            if (input.equals("")) {
+                break;
+            }
+            list.add(input);
+        }
+        System.out.println(list.get(2));
+    }
+}
---
+++ src/Part_I/Module_03/Ex_02_SecondPlusThird/SecondPlusThird.java
@@ -0,0 +1,7 @@
+package Part_I.Module_03.Ex_02_SecondPlusThird;
+
+public class SecondPlusThird {
+    public static void main(String[] args) {
+
+    }
+}
