2efe07bd45262a332c3d06fa9abe3975b36cc652
john <jonhmiguel138@gmail.com>
2026-04-03 20:34:52 -0300

    feat: Resolution of question 11 - IndexOfSmallest of module 3

--- src/Part_I/Module_03/Ex_11_IndexOfSmallest/IndexOfSmallest.java
+++ src/Part_I/Module_03/Ex_11_IndexOfSmallest/IndexOfSmallest.java
@@ -1,25 +1,25 @@
-package Part_I.Module_03.Ex_10_IndexOf;
+package Part_I.Module_03.Ex_11_IndexOfSmallest;
 
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class IndexOf {
+public class IndexOfSmallest {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         ArrayList<Integer> numbers = new ArrayList<>();
         while (true){
             int number = Integer.parseInt(sc.nextLine());
-            if (number == -1){
+            if (number == 9999){
                 break;
             }
             numbers.add(number);
         }
         if (!(numbers.size() == 0)){
-            System.out.print("Search for: ");
-            int search = Integer.parseInt(sc.nextLine());
+            System.out.print("Smallest number: ");
+            int smallest = Integer.parseInt(sc.nextLine());
             for (int i = 0; i < numbers.size(); i++) {
-                if (search == numbers.get(i)){
-                    System.out.println(search + " is at " + i + " index");
+                if (smallest == numbers.get(i)){
+                    System.out.println("Found at the index: " + i);
                 }
             }
         }
