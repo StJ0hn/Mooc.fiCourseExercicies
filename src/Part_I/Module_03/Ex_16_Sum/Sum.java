e6460fdf2ac5acc7d7c324db4e485a7a216036fa
john <jonhmiguel138@gmail.com>
2026-04-07 16:17:43 -0300

    feat: Resolution of question 16 - Sum of module 3

---
+++ src/Part_I/Module_03/Ex_16_Sum/Sum.java
@@ -0,0 +1,26 @@
+package Part_I.Module_03.Ex_16_Sum;
+
+import java.util.ArrayList;
+
+public class Sum {
+    public static void main(String[] args) {
+        ArrayList<Integer> numbers = new ArrayList<>();
+        numbers.add(3);
+        numbers.add(2);
+        numbers.add(6);
+        numbers.add(-1);
+        System.out.println(sum(numbers));
+
+        numbers.add(5);
+        numbers.add(1);
+        System.out.println(sum(numbers));
+    }
+
+    public static int sum(ArrayList<Integer> numbers){
+        int sum = 0;
+        for (int number : numbers){
+            sum+=number;
+        }
+        return sum;
+    }
+}
