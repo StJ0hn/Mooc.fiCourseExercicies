f7c2ca52a3257d65f05dacfab43816084d6a62dd
john <jonhmiguel138@gmail.com>
2026-04-09 18:26:46 -0300

    fix: Resolution of question 21 - PrintNeatly of module 3

--- src/Part_I/Module_03/Ex_21_PrintNeatly/PrintNeatly.java
+++ src/Part_I/Module_03/Ex_21_PrintNeatly/PrintNeatly.java
@@ -1,4 +1,16 @@
 package Part_I.Module_03.Ex_21_PrintNeatly;
 
 public class PrintNeatly {
+    public static void main(String[] args) {
+        int[] array = {5, 1, 3, 4, 2};
+        printNeatly(array);
+    }
+    public static void printNeatly(int[] array){
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i]);
+            if (array[i] != array[array.length - 1]){
+                System.out.print(", ");
+            }
+        }
+    }
 }
