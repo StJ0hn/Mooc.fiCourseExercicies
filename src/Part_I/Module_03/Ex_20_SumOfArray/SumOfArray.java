fdb8242ed2aade30a1a2fd5f55116fc7a34435f5
john <jonhmiguel138@gmail.com>
2026-04-09 09:28:27 -0300

    feat: Resolution of question 20 - SumOfArray of module 3

--- src/Part_I/Module_03/Ex_20_SumOfArray/SumOfArray.java
+++ src/Part_I/Module_03/Ex_20_SumOfArray/SumOfArray.java
@@ -1,4 +1,15 @@
 package Part_I.Module_03.Ex_20_SumOfArray;
 
 public class SumOfArray {
+    public static void main(String[] args) {
+        int[] numbers = {5, 1, 3, 4, 2};
+        System.out.println(sumOfNumbersInArray(numbers));
+    }
+    public static int sumOfNumbersInArray(int[] numbers){
+        int sum = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            sum += numbers[i];
+        }
+        return sum;
+    }
 }
