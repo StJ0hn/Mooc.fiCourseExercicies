48bb1884654c463aece7a72fcca9b6097af20724
john <jonhmiguel138@gmail.com>
2026-04-13 16:19:56 -0300

    feat: Resolution of question 30 - AgeOfTheOldest of module 3

---
+++ src/Part_I/Module_03/Ex_30_AgeOfTheOldest/AgeOfTheOldest.java
@@ -0,0 +1,19 @@
+package Part_I.Module_03.Ex_30_AgeOfTheOldest;
+
+import java.util.Scanner;
+
+public class AgeOfTheOldest {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            String text = sc.nextLine();
+            if (text.isBlank()) {
+                break;
+            }
+            String[] pieces = text.split(",");
+            for (String piece : pieces) {
+
+            }
+        }
+    }
+}
