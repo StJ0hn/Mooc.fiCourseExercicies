38681a6bacd88c6abff139fbe65f5a03c25d9f6f
john <jonhmiguel138@gmail.com>
2026-04-14 13:06:08 -0300

    fix: Resolution of question 30 - AgeOfTheOldest of module 3

--- src/Part_I/Module_03/Ex_30_AgeOfTheOldest/AgeOfTheOldest.java
+++ src/Part_I/Module_03/Ex_30_AgeOfTheOldest/AgeOfTheOldest.java
@@ -5,15 +5,21 @@ import java.util.Scanner;
 public class AgeOfTheOldest {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        String nameOfGreatest = "";
+        int greatest = 0;
         while (true) {
             String text = sc.nextLine();
             if (text.isBlank()) {
                 break;
             }
-            String[] pieces = text.split(",");
-            for (String piece : pieces) {
 
+            String[] pieces = text.split(",");
+            int parsedAge = Integer.parseInt(pieces[1]);
+            if (parsedAge > greatest){
+                greatest = parsedAge;
+                nameOfGreatest = pieces[0];
             }
         }
+        System.out.print("Name of the oldest: " + nameOfGreatest);
     }
 }
