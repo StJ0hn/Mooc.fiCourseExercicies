bcfa146062f887b12d84ecc241f3eeb56092308b
john <jonhmiguel138@gmail.com>
2026-05-11 12:16:17 -0300

    feat: Resolution of question 14 - Multiplier of module 4

---
+++ src/Part_I/Module_04/Ex_14_Multiplier/Multiplier.java
@@ -0,0 +1,13 @@
+package Part_I.Module_04.Ex_14_Multiplier;
+
+public class Multiplier {
+    private int number;
+
+    public Multiplier(int number) {
+        this.number = number;
+    }
+
+    public int multiply (int number){
+        return number * this.number;
+    }
+}
