45183852b479c002c65722071b69c86a5860880d
john <jonhmiguel138@gmail.com>
2026-05-22 19:12:48 -0300

    feat: resolution of question 05 - Constructor Overload of module 5

---
+++ src/Part_I/Module_05/Ex_05_ConstructorOverload/Product.java
@@ -0,0 +1,30 @@
+package Part_I.Module_05.Ex_05_ConstructorOverload;
+
+public class Product {
+    private String name;
+    private String location;
+    private int weight;
+
+    public Product(String name, String location, int weight){
+        this.name = name;
+        this.location = location;
+        this.weight = weight;
+    }
+
+    public Product (String name){
+        this(name, "Shelf", 1);
+    }
+
+    public Product(String name, String location){
+        this(name, location, 1);
+    }
+
+    public Product(String name, int weight){
+        this(name, "Shelf", weight);
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
