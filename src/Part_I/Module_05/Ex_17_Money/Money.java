80802c0d1880561c067eea2fbe0b297be245b90b
john <jonhmiguel138@gmail.com>
2026-05-31 23:48:57 -0300

    fix: resolution of question 17 - Money of module 5

--- src/Part_I/Module_05/Ex_17_Money/Money.java
+++ src/Part_I/Module_05/Ex_17_Money/Money.java
@@ -19,33 +19,49 @@ public class Money {
 
     public String toString() {
         String zero = "";
-        if (cents <= 10) {
+        if (cents < 10) {
             zero = "0";
         }
-
         return euros + "." + zero + cents + "e";
     }
 
-    public Money plus(Money addition){
-        Money newMoney = new Money(euros + addition.euros, cents + addition.cents);
-        return newMoney;
+    public Money plus(Money addition) {
+        int newEuros = this.euros + addition.euros;
+        int newCents = this.cents + addition.cents;
+
+        if (newCents > 99) {
+            newCents -= 100;
+            newEuros++;
+        }
+
+        return new Money(newEuros, newCents);
     }
 
-    public boolean lessThan(Money compared){
-        Money newMoney = new Money(euros, cents);
-        if (newMoney.euros < compared.euros){
+    public boolean lessThan(Money compared) {
+        if (this.euros < compared.euros) {
             return true;
         }
-        if (newMoney.euros == compared.euros && newMoney.cents < compared.cents){
+
+        if (this.euros == compared.euros && this.cents < compared.cents) {
             return true;
         }
+
         return false;
     }
 
-    public Money minus(Money decreaser){
-        Money newMoney = new Money(euros - decreaser.euros, cents - decreaser.cents);
-        if (newMoney.euros < 0)
-            newMoney = new Money(0,0);
-        return newMoney;
+    public Money minus(Money decreaser) {
+        int newEuros = this.euros - decreaser.euros;
+        int newCents = this.cents - decreaser.cents;
+
+        if (newCents < 0) {
+            newCents += 100;
+            newEuros--;
+        }
+
+        if (newEuros < 0) {
+            return new Money(0, 0);
+        }
+
+        return new Money(newEuros, newCents);
     }
 }
